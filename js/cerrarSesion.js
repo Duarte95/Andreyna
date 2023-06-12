e44c4e842692e91eb04d7309a9692ff44707a70d
Duarte95 <122475491+Duarte95@users.noreply.github.com>
2023-06-11 20:11:25 -0400

    puliendo codigo

--- js/cerrarSesion.js
+++
@@ -1,5 +0,0 @@
-const btnSalir = document.querySelector("[data-salir]");
-
-btnSalir.addEventListener("click", (e)=>{
-  console.log("Hello World");
-})
---
+++ js/seleccion.js
@@ -0,0 +1,15 @@
+const options = document.querySelectorAll('.departamentos');
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    if (option.classList.contains('selected')) {
+      option.classList.remove('selected');
+    } else {
+      options.forEach(option => {
+        option.classList.remove('selected');
+      });
+      option.classList.add('selected');
+    }
+  });
+});
+
