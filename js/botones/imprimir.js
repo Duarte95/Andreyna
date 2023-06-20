99f261baf99c15ef3b5450c0f4d8e5a91e6ce278
Duarte95 <122475491+Duarte95@users.noreply.github.com>
2023-06-20 01:07:20 -0400

    insertando archivos pdf

--- js/botones/imprimir.js
+++ js/botones/imprimir.js
@@ -1,35 +1,71 @@
-// Ruta al archivo PDF
-const pdfUrl = "../../files/antony.pdf";
+// // Ruta al archivo PDF
+// const pdfUrl = "../../files/antony.pdf";
 
-// Elemento contenedor del PDF
-const container = document.getElementById("pdfContainer");
+// // Elemento contenedor del PDF
+// const container = document.getElementById("pdfContainer");
 
-// Cargar el archivo PDF
-pdfjsLib.getDocument(pdfUrl).promise.then(function (pdf) {
-  // Obtener la primera página del PDF
-  pdf.getPage(1).then(function (page) {
-    const scale = 1.5;
-    const viewport = page.getViewport({ scale });
+// // Cargar el archivo PDF
+// pdfjsLib.getDocument(pdfUrl).promise.then(function (pdf) {
+//   // Obtener la primera página del PDF
+//   pdf.getPage(1).then(function (page) {
+//     const scale = 1.5;
+//     const viewport = page.getViewport({ scale });
 
-    // Crear lienzo para mostrar la página
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
-    container.appendChild(canvas);
+//     // Crear lienzo para mostrar la página
+//     const canvas = document.createElement("canvas");
+//     const context = canvas.getContext("2d");
+//     canvas.width = viewport.width;
+//     canvas.height = viewport.height;
+//     container.appendChild(canvas);
 
-    // Renderizar la página en el lienzo
-    const renderContext = {
-      canvasContext: context,
-      viewport: viewport,
-    };
-    page.render(renderContext);
+//     // Renderizar la página en el lienzo
+//     const renderContext = {
+//       canvasContext: context,
+//       viewport: viewport,
+//     };
+//     page.render(renderContext);
+//   });
+// });
+
+// function imprimirArchivo() {
+//   const iframe = document.createElement("iframe");
+//   iframe.src = pdfUrl;
+//   iframe.style.display = "none";
+//   document.body.appendChild(iframe);
+//   iframe.contentWindow.print();
+// }
+
+function cargarArchivoPDF(urlArchivo) {
+  // Elemento contenedor del PDF
+  const container = document.getElementById("pdfContainer");
+
+  // Cargar el archivo PDF
+  pdfjsLib.getDocument(urlArchivo).promise.then(function (pdf) {
+    // Obtener la primera página del PDF
+    pdf.getPage(1).then(function (page) {
+      const scale = 1.5;
+      const viewport = page.getViewport({ scale });
+
+      // Crear lienzo para mostrar la página
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+      container.appendChild(canvas);
+
+      // Renderizar la página en el lienzo
+      const renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+      };
+      page.render(renderContext);
+    });
   });
-});
+}
 
-function imprimirArchivo() {
+function imprimirArchivo(urlArchivo) {
   const iframe = document.createElement("iframe");
-  iframe.src = pdfUrl;
+  iframe.src = urlArchivo;
   iframe.style.display = "none";
   document.body.appendChild(iframe);
   iframe.contentWindow.print();
