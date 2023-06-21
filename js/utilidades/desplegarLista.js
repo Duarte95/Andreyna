f6f67498fae0475018fb9e6dbbfe32f36a8045d4
Duarte95 <antonyduarte.edu@gmail.com>
2023-06-21 07:11:08 -0400

    acomodando js

--- js/utilidades/desplegarLista.js
+++ js/utilidades/desplegarLista.js
@@ -1,4 +1,4 @@
-// Obtener todos los elementos de categoría
+/*// Obtener todos los elementos de categoría
   const categorias = document.querySelectorAll('.categoria');
   const contenedor = document.querySelectorAll('.dropdown-content');
   
@@ -8,5 +8,22 @@ categorias.forEach(categoria => {
     const contenedores = categoria.nextElementSibling;
     // Cambiar la clase 'active' a la categoría seleccionada
     contenedores.classList.toggle('show');
+  });
+});*/
+
+// Obtener todos los elementos de categoría
+const categorias = document.querySelectorAll(".categoria");
+const contenedores = document.querySelectorAll(".dropdown-content");
+
+// Agregar el evento click a cada categoría
+categorias.forEach((categoria, index) => {
+  categoria.addEventListener("click", () => {
+    // Desactivar la clase 'show' en todos los contenedores
+    contenedores.forEach((contenedor) => {
+      contenedor.classList.remove("show");
+    });
+
+    // Activar la clase 'show' en el contenedor correspondiente
+    contenedores[index].classList.add("show");
   });
 });
