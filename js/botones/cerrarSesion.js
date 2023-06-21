45c7c55703226c1554649aa598250f2be489dcb8
antonyduarte.edu@gmail.com <antonyduarte.edu@gmail.com>
2023-06-20 23:11:05 -0400

    ajuste de cerrar sesion y procesos

--- js/botones/cerrarSesion.js
+++ js/botones/cerrarSesion.js
@@ -1,4 +1,4 @@
-const botonSalir = document.querySelector("[data-salir]");
+/*const botonSalir = document.querySelector("[data-salir]");
 
 botonSalir.addEventListener("click", function () {
   // Realizar acciones de cierre de sesión, como eliminar datos de sesión, limpiar cookies, etc.
@@ -8,6 +8,33 @@ botonSalir.addEventListener("click", function () {
   window.location.href = "../pantallas/seccion_finalizado.html";
   // Reemplazar la URL actual en el historial del navegador
   history.replaceState(null, null, "../pantallas/ingreso_sistema.html");
-});
+});*/
 
 ///../seccion_finalizado.html'; agregar esto para definiciones y establecer la condicion
+
+const botonSalir = document.querySelector('[data-salir]');
+
+botonSalir.addEventListener('click', function() {
+  // Realizar acciones de cierre de sesión
+  
+  sessionStorage.removeItem('sesionIniciada'); // Eliminar indicador de inicio de sesión de sessionStorage
+  
+  // Obtener la ruta relativa actual del archivo
+  const rutaActual = window.location.pathname;
+  
+  // Reemplazar la ruta según tus necesidades
+  let nuevaRuta = '';
+  
+  // Verificar si la ruta actual está en una subcarpeta
+  if (rutaActual.includes('/definiciones/')) {
+    nuevaRuta = '../seccion_finalizado.html';
+  } else {
+    nuevaRuta = '../pantallas/seccion_finalizado.html';
+  }
+  
+  // Redireccionar al usuario a la nueva ruta
+  window.location.href = nuevaRuta;
+  
+  // Reemplazar la URL actual en el historial del navegador
+  history.replaceState(null, null, '../pantallas/ingreso_sistema.html');
+});
--- js/utilidades/desplegarLista.js
+++ js/utilidades/desplegarLista.js
@@ -1,9 +1,12 @@
-function toggleSubProcesos(categoria) {
-  const subProcesos = categoria.nextElementSibling;
-
-  if (subProcesos.style.display === "none") {
-    subProcesos.style.display = "block";
-  } else {
-    subProcesos.style.display = "none";
-  }
-}
+// Obtener todos los elementos de categoría
+  const categorias = document.querySelectorAll('.categoria');
+  const contenedor = document.querySelectorAll('.dropdown-content');
+  
+// Agregar el evento click a cada categoría
+categorias.forEach(categoria => {
+  categoria.addEventListener('click', () => {
+    const contenedores = categoria.nextElementSibling;
+    // Cambiar la clase 'active' a la categoría seleccionada
+    contenedores.classList.toggle('show');
+  });
+});
