44d5cae88f87a4e3fc636b9339606032e338c422
antonyduarte.edu@gmail.com <antonyduarte.edu@gmail.com>
2023-06-21 20:13:42 -0400

    ajustando imprimir

--- js/botones/imprimir.js
+++ js/botones/imprimir.js
@@ -1,3 +1,4 @@
+/*
 function cargarArchivoPDF(urlArchivo) {
   // Elemento contenedor del PDF
   const container = document.getElementById("pdfContainer");
@@ -32,4 +33,12 @@ function imprimirArchivo(urlArchivo) {
   iframe.style.display = "none";
   document.body.appendChild(iframe);
   iframe.contentWindow.print();
+}
+*/
+
+function imprimirArchivo(urlArchivo) {
+  const nuevaVentana = window.open(urlArchivo, "_blank");
+  nuevaVentana.addEventListener("load", function() {
+    nuevaVentana.print();
+  });
 }
