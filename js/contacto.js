127090bb1bb603b1ac3d38b3057ecfda6b12817e
antonyduarte.edu@gmail.com <antonyduarte.edu@gmail.com>
2023-06-24 12:04:06 -0400

    ajuste de contacto

---
+++ js/contacto.js
@@ -0,0 +1,11 @@
+function git() {
+  window.open('https://github.com/Duarte95');
+}
+
+function correo() {
+  window.open('https://github.com/Duarte95');
+}
+
+function linkedin(){
+  window.open('https://linkedin.com/')
+}
