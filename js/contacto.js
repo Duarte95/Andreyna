a22b90ed83b010e1a45477d05b6fe4a06c31cda8
Duarte95 <antonyduarte.edu@gmail.com>
2023-06-27 13:07:28 -0400

    reenvio a correo

--- js/contacto.js
+++ js/contacto.js
@@ -1,11 +1,31 @@
 function git() {
-  window.open('https://github.com/Duarte95');
+  window.open("https://github.com/Duarte95");
 }
 
 function correo() {
-  window.open('https://github.com/Duarte95');
+  window.open("https://github.com/Duarte95");
 }
 
-function linkedin(){
-  window.open('https://linkedin.com/')
+function linkedin() {
+  window.open("https://linkedin.com/");
 }
+
+/*
+<a href="https://mail.google.com/mail/?view=cm&fs=1&to=" id="enviarCorreo">Enviar correo</a>
+
+<input type="text" id="asunto" name="asunto" placeholder="Asunto">
+<textarea id="mensaje" name="mensaje" placeholder="Mensaje"></textarea>
+
+document.getElementById("enviarCorreo").addEventListener("click", function(event) {
+  event.preventDefault();  // Evita el comportamiento predeterminado del enlace
+
+  var correoDestino = "correo@gmail.com";  // Aquí puedes establecer el correo de destino deseado
+  var asunto = encodeURIComponent(document.getElementById("asunto").value);
+  var mensaje = encodeURIComponent(document.getElementById("mensaje").value);
+
+  var url = "https://mail.google.com/mail/?view=cm&fs=1&to=" + correoDestino +
+            "&su=" + asunto + "&body=" + mensaje;
+
+  window.open(url);  // Abre una nueva ventana o pestaña con la URL de Gmail prellenada
+});
+*/
